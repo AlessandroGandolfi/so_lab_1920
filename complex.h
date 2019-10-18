893ad2de63ed3060523b468656f446672dc7f138
Alessandro Gandolfi <alessandro.gando98@gmail.com>
2019-10-18 10:12:40 +0200

    corretto uso e inclusione libreria complex

--- complex.h
+++ complex.h
@@ -1,3 +1,11 @@
+// per compilazione file .h con riferimento ad altre librerie uso parametri -c -lm
+// per compilazione file .c uso parametri -o {nome output} -lm
+
+// funzioni matematiche
+#include <math.h>
+// funzioni allocazione
+#include <stdlib.h>
+
 // esempio struttura numero complesso
 typedef struct _complex {
     double i; // parte immaginaria
@@ -12,6 +20,55 @@ double complex_getImaginary(complex);
 complex complex_sum(complex, complex);
 complex complex_sub(complex, complex);
 complex complex_prod(complex, complex);
-
 // es5
 complex *complex_sum_ptr(const complex *, const complex *);
+
+complex complex_new(double r, double i) {
+    complex new;
+    new.r = r;
+    new.i = i;
+    return new;
+}
+
+double complex_modulo(complex a) {
+    return sqrt(a.i * a.i + a.r * a.r); // bisogna includere math.h per utilizzo sqrt()
+                                        // man7.org per manuale, usare -lm per corretta compilazione/collegamento con libreria
+}
+
+double complex_getReal(complex a) {
+    return a.r;
+}
+
+double complex_getImaginary(complex a) {
+    return a.i;
+}
+
+complex complex_sum(complex a, complex b) {
+    complex c;
+    c.i = a.i + b.i;
+    c.r = a.r + b.r;
+    return c;
+}
+
+complex complex_sub(complex a, complex b) {
+    complex c;
+    c.i = a.i - b.i;
+    c.r = a.r - b.r;
+    return c;
+}
+
+complex complex_prod(complex a, complex b) {
+    complex c;
+    c.r = a.r * b.r - a.i * b.i;
+    c.i = a.i * b.i - a.r * b.r;
+    return c;
+}
+
+// es5
+// dopo aver richiamato la funzione dovrá essere usata la free() sulla variabile contenente il valore restituito
+complex *complex_sum_ptr(const complex *a, const complex *b) {
+    complex *c = (complex *) malloc(sizeof(complex));
+    c->r = a->r + (*b).r; // a->r = (*a).r
+    c->i = a->i + b->i;
+    return c; 
+}
--- es3_complexnum.c
+++ es3_complexnum.c
@@ -1,5 +1,6 @@
-#include <math.h>
+// libreria per printf
 #include <stdio.h>
+
 // definizione struttura e firme funzioni
 #include "./complex.h"
 
@@ -11,54 +12,4 @@ int main() {
     sum = complex_sum(n1, n2);
 
     printf("La somma é %f + %f.i\n", complex_getReal(sum), complex_getImaginary(sum));
-}
-
-complex complex_new(double r, double i) {
-    complex new;
-    new.r = r;
-    new.i = i;
-    return new;
-}
-
-double complex_modulo(complex a) {
-    return sqrt(a.i * a.i + a.r * a.r); // bisogna includere math.h per utilizzo sqrt()
-                                        // man7.org per manuale, usare -lm per corretta compilazione/collegamento con libreria
-}
-
-double complex_getReal(complex a) {
-    return a.r;
-}
-
-double complex_getImaginary(complex a) {
-    return a.i;
-}
-
-complex complex_sum(complex a, complex b) {
-    complex c;
-    c.i = a.i + b.i;
-    c.r = a.r + b.r;
-    return c;
-}
-
-complex complex_sub(complex a, complex b) {
-    complex c;
-    c.i = a.i - b.i;
-    c.r = a.r - b.r;
-    return c;
-}
-
-complex complex_prod(complex a, complex b) {
-    complex c;
-    c.r = a.r * b.r - a.i * b.i;
-    c.i = a.i * b.i - a.r * b.r;
-    return c;
-}
-
-// es5
-// dopo aver richiamato la funzione dovrá essere usata la free() sulla variabile contenente il valore restituito
-complex *complex_sum_ptr(const complex *a, const complex *b) {
-    complex *c = (complex *) malloc(sizeof(complex));
-    c->r = a->r + (*b).r; // a->r = (*a).r
-    c->i = a->i + b->i;
-    return c; 
 }
